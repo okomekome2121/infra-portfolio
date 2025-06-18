acdda48c5b026f4163129a3ffbea080655f1f24d
okomekome2121 <165615767+okomekome2121@users.noreply.github.com>
2025-06-18 18:28:56 +0900

    Delete security_groups directory

--- security_groups/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_sg_id" {
-  description = "ALB security group ID"
-  value       = aws_security_group.alb_sg.id
-}
--- security_groups/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "ID of the VPC"
-  type        = string
-}
-
-variable "name_prefix" {
-  description = "Prefix for resource names"
-  type        = string
-}
