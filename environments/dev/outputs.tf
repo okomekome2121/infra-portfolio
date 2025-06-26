f3d9ad15df10a36d3fbea4e8e3beec0c44853be0
okome <okomekome.2121@gmail.com>
2025-06-26 16:55:57 +0900

    Add output.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,3 +1,23 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.albvpc.vpc_id
+}
+
+output "alb_arn" {
+  description = "ALBのARN"
+  value       = module.alb.aws_lb.main.arn
+}
+
+output "alb_zone_id" {
+  description = "ALBのゾーンID"
+  value       = module.albaws_lb.main.zone_id
+}
+
+output "target_group_arn" {
+  description = "ターゲットグループのARN"
+  value       = module.albaws_lb_target_group.main.arn
+}
+
+output "listener_arn" {
+  description = "リスナーのARN"
+  value       = module.albaws_lb_listener.http.arn
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -7,3 +7,75 @@ variable "name_prefix" {
   default     = "my-vpc"
   description = "リソース名に使うプレフィックス"
 }
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "リソースに付与するタグ"
+}
+
+variable "enable_deletion_protection" {
+  type        = bool
+  default     = false
+  description = "削除保護の有効化"
+}
+
+variable "target_group_protocol" {
+  type        = string
+  default     = "HTTP"
+  description = "ターゲットグループのプロトコル"
+}
+
+variable "health_check_path" {
+  type        = string
+  default     = "/"
+  description = "ヘルスチェックのパス"
+}
+
+variable "health_check_protocol" {
+  type        = string
+  default     = "HTTP"
+  description = "ヘルスチェックのプロトコル"
+}
+
+variable "health_check_matcher" {
+  type        = string
+  default     = "200"
+  description = "ヘルスチェックのマッチャー"
+}
+
+variable "health_check_interval" {
+  type        = number
+  default     = 30
+  description = "ヘルスチェックの間隔"
+}
+
+variable "health_check_timeout" {
+  type        = number
+  default     = 5
+  description = "ヘルスチェックのタイムアウト"
+}
+
+variable "healthy_threshold" {
+  type        = number
+  default     = 2
+  description = "正常判定までの回数"
+}
+
+variable "unhealthy_threshold" {
+  type        = number
+  default     = 2
+  description = "異常判定までの回数"
+}
+
+variable "listener_port" {
+  type        = number
+  default     = 80
+  description = "リスナーのポート番号"
+}
+
+variable "listener_protocol" {
+  type        = string
+  default     = "HTTP"
+  description = "リスナーのプロトコル"
+}
